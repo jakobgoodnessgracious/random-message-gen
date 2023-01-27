f97031b35d67c8f6e78264e19ff3c52c78fbc31c
Jakob Giese <gooserjg@hotmail.com>
2023-01-27 17:04:54 -0500

    latest

--- random-message-gen.js
+++ random-message-gen.js
@@ -1,9 +1,9 @@
 const genRandomNumInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const baseString = 'You should:';
-const verbs = ['go', 'run', 'crawl', 'walk', 'cry', 'wince', 'daydream', 'ponder', 'meditate'];
-const prepostionToVerb = {at: ['cry', 'daydream', 'ponder', 'meditate', 'wince'], to: ['go', 'run', 'crawl', 'walk', 'wince']}
-const places = ['the grocery store', 'the park', 'the mall', 'the gas station', 'work', 'home'];
+const verbs = ['go', 'run', 'crawl', 'walk', 'cry', 'wince', 'daydream', 'sex', 'ponder', 'meditate'];
+const prepostionToVerb = {in: ['sex'], at: ['cry', 'daydream', 'ponder', 'meditate', 'wince'], to: ['go', 'run', 'crawl', 'walk', 'wince']}
+const places = ['the grocery store', 'the park', 'the mall', 'the gas station', 'work', 'home', 'the bedroom'];
 
 const getRandomValueFromArray = (array) => {
     return array[genRandomNumInRange(0, array.length - 1)];
